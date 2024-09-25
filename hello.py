33ff0a148e09c7363c8e0962738e73291e790c6e
abhishekmadan7 <95368860+abhishekmadan7@users.noreply.github.com>
2024-09-25 13:38:43 -0400

    Added nav bar, error pages and date and time check

--- hello.py
+++ hello.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_moment import Moment
+from datetime import datetime
+from flask_bootstrap import Bootstrap
+
+
+
 app = Flask(__name__)
+moment = Moment(app)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html',current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html',name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+
+
